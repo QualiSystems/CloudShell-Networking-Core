bce4284f0f0f66e1ec34fb0af6ef4e62e7d10c44
CoYe <wiseman.kostya@gmail.com>
2016-08-25 17:34:01 +0300

    temporary comment out orchestration save/restore

--- cloudshell/networking/operations/configuration_operations.py
+++ cloudshell/networking/operations/configuration_operations.py
@@ -59,51 +59,52 @@ class ConfigurationOperations(ConfigurationOperationsInterface):
         :rtype json
         """
 
-        if custom_params is None or custom_params == '':
-            raise Exception('ConfigurationOperations', 'request is None or empty')
-
-        params = JsonRequestDeserializer(jsonpickle.decode(custom_params))
-
-        if not params:
-            raise Exception('ConfigurationOperations', 'Deserialized custom_params is None or empty')
-
-        save_params = dict()
-
-        if hasattr(params, 'custom_params'):
-            if hasattr(params.custom_params, 'configuration_type'):
-                save_params['configuration_type'] = params.custom_params.configuration_type
-
-            if hasattr(params.custom_params, 'folder_path'):
-                save_params['folder_path'] = params.custom_params.folder_path
-            else:
-                save_params['folder_path'] = get_attribute_by_name('Backup Location')
-                if not save_params['folder_path']:
-                    raise Exception('ConfigurationOperations', 'Folder Path is empty')
-
-            url = UrlParser.parse_url(save_params['folder_path'])
-            if UrlParser.SCHEME in url and UrlParser.HOSTNAME in url:
-                artifact_type = url[UrlParser.SCHEME]
-                server_address = url[UrlParser.HOSTNAME]
-            else:
-                raise Exception('ConfigurationOperations', 'Cannot retrieve artifact type')
-
-            if hasattr(params.custom_params, 'vrf_management_name'):
-                save_params['vrf_management_name'] = params.custom_params.vrf_management_name
-
-        self.logger.info('Action: ', params.__dict__)
-        result = self.save(**save_params)
-
-        saved_artifact = OrchestrationSavedArtifact(identifier=result, artifact_type=artifact_type,
-                                                    server=server_address, ftp_resource='')
-
-        saved_artifact_info = OrchestrationSavedArtifactInfo(resource_name=self.resource_name,
-                                                             created_date=_get_snapshot_time_stamp(),
-                                                             restore_rules=self.get_restore_rules(),
-                                                             saved_artifact=saved_artifact)
-        save_response = OrchestrationSaveResult(saved_artifacts_info=saved_artifact_info)
-        self._validate_artifact_info(saved_artifact_info)
-
-        return set_command_result(save_response)
+        # if custom_params is None or custom_params == '':
+        #     raise Exception('ConfigurationOperations', 'request is None or empty')
+        #
+        # params = JsonRequestDeserializer(jsonpickle.decode(custom_params))
+        #
+        # if not params:
+        #     raise Exception('ConfigurationOperations', 'Deserialized custom_params is None or empty')
+        #
+        # save_params = dict()
+        #
+        # if hasattr(params, 'custom_params'):
+        #     if hasattr(params.custom_params, 'configuration_type'):
+        #         save_params['configuration_type'] = params.custom_params.configuration_type
+        #
+        #     if hasattr(params.custom_params, 'folder_path'):
+        #         save_params['folder_path'] = params.custom_params.folder_path
+        #     else:
+        #         save_params['folder_path'] = get_attribute_by_name('Backup Location')
+        #         if not save_params['folder_path']:
+        #             raise Exception('ConfigurationOperations', 'Folder Path is empty')
+        #
+        #     url = UrlParser.parse_url(save_params['folder_path'])
+        #     if UrlParser.SCHEME in url and UrlParser.HOSTNAME in url:
+        #         artifact_type = url[UrlParser.SCHEME]
+        #         server_address = url[UrlParser.HOSTNAME]
+        #     else:
+        #         raise Exception('ConfigurationOperations', 'Cannot retrieve artifact type')
+        #
+        #     if hasattr(params.custom_params, 'vrf_management_name'):
+        #         save_params['vrf_management_name'] = params.custom_params.vrf_management_name
+        #
+        # self.logger.info('Action: ', params.__dict__)
+        # result = self.save(**save_params)
+        #
+        # saved_artifact = OrchestrationSavedArtifact(identifier=result, artifact_type=artifact_type,
+        #                                             server=server_address, ftp_resource='')
+        #
+        # saved_artifact_info = OrchestrationSavedArtifactInfo(resource_name=self.resource_name,
+        #                                                      created_date=_get_snapshot_time_stamp(),
+        #                                                      restore_rules=self.get_restore_rules(),
+        #                                                      saved_artifact=saved_artifact)
+        # save_response = OrchestrationSaveResult(saved_artifacts_info=saved_artifact_info)
+        # self._validate_artifact_info(saved_artifact_info)
+        #
+        # return set_command_result(save_response)
+        pass
 
     def orchestration_restore(self, saved_artifact_info, custom_params=None):
         """Orchestration restore
@@ -114,57 +115,58 @@ class ConfigurationOperations(ConfigurationOperationsInterface):
         :rtype json
         """
 
-        restore_params = {'configuration_type': 'running'}
-
-        if saved_artifact_info is None or saved_artifact_info == '':
-            raise Exception('ConfigurationOperations', 'saved_artifact_info is None or empty')
-
-        saved_artifact_info = JsonRequestDeserializer(jsonpickle.decode(saved_artifact_info))
-        if not hasattr(saved_artifact_info, 'saved_artifacts_info'):
-            raise Exception('ConfigurationOperations', 'Saved_artifacts_info is missing')
-        saved_config = saved_artifact_info.saved_artifacts_info
-        params = ''
-        if custom_params:
-            params = jsonpickle.decode(custom_params)
-            self._validate_custom_params(params)
-
-        self._validate_artifact_info(saved_config)
-
-        if saved_config.restore_rules.requires_same_resource \
-                and saved_config.resource_name.lower() != self.resource_name.lower():
-            raise Exception('ConfigurationOperations', 'Incompatible resource, expected {}'.format(self.resource_name))
-
-        url = UrlParser.parse_url(saved_config.saved_artifact.identifier)
-        url[UrlParser.SCHEME] = saved_config.saved_artifact.artifact_type
-        url[UrlParser.HOSTNAME] = saved_config.saved_artifact.server
-        if url[UrlParser.SCHEME] == 'ftp' and saved_config.saved_artifact.ftp_resource:
-            url[UrlParser.USERNAME] = self._get_resource_attribute(saved_config.saved_artifact.ftp_resource, 'User')
-            url[UrlParser.PASSWORD] = self._get_resource_attribute(saved_config.saved_artifact.ftp_resource, 'Password')
-
-        if hasattr(params, 'custom_params'):
-            if hasattr(params.custom_params, 'path'):
-                restore_params['path'] = params.custom_params.path
-            if hasattr(params.custom_params, 'configuration_type'):
-                restore_params['configuration_type'] = params.custom_params.configuration_type
-            if hasattr(params.custom_params, 'restore_method'):
-                restore_params['restore_method'] = params.custom_params.restore_method
-            if hasattr(params.custom_params, 'vrf_management_name'):
-                restore_params['vrf_management_name'] = params.custom_params.vrf_management_name
-        else:
-            restore_params['restore_method'] = 'override'
-            restore_params['configuration_type'] = 'running'
-            if UrlParser.FILENAME in url and url[UrlParser.FILENAME] and 'startup' in url[UrlParser.FILENAME]:
-                restore_params['configuration_type'] = 'startup'
-            restore_params['vrf_management_name'] = self._get_resource_attribute(self.resource_name,
-                                                                                 'VRF Manaagement Name') or ''
-        try:
-            restore_params['path'] = UrlParser.build_url(**url)
-        except Exception as e:
-            self.logger.error('Failed to build url: {}'.format(e))
-            if 'path' not in restore_params and not restore_params['path']:
-                raise Exception('ConfigurationOperations', 'custom_params is None or empty')
-
-        self.restore(**restore_params)
+        # restore_params = {'configuration_type': 'running'}
+        #
+        # if saved_artifact_info is None or saved_artifact_info == '':
+        #     raise Exception('ConfigurationOperations', 'saved_artifact_info is None or empty')
+        #
+        # saved_artifact_info = JsonRequestDeserializer(jsonpickle.decode(saved_artifact_info))
+        # if not hasattr(saved_artifact_info, 'saved_artifacts_info'):
+        #     raise Exception('ConfigurationOperations', 'Saved_artifacts_info is missing')
+        # saved_config = saved_artifact_info.saved_artifacts_info
+        # params = ''
+        # if custom_params:
+        #     params = jsonpickle.decode(custom_params)
+        #     self._validate_custom_params(params)
+        #
+        # self._validate_artifact_info(saved_config)
+        #
+        # if saved_config.restore_rules.requires_same_resource \
+        #         and saved_config.resource_name.lower() != self.resource_name.lower():
+        #     raise Exception('ConfigurationOperations', 'Incompatible resource, expected {}'.format(self.resource_name))
+        #
+        # url = UrlParser.parse_url(saved_config.saved_artifact.identifier)
+        # url[UrlParser.SCHEME] = saved_config.saved_artifact.artifact_type
+        # url[UrlParser.HOSTNAME] = saved_config.saved_artifact.server
+        # if url[UrlParser.SCHEME] == 'ftp' and saved_config.saved_artifact.ftp_resource:
+        #     url[UrlParser.USERNAME] = self._get_resource_attribute(saved_config.saved_artifact.ftp_resource, 'User')
+        #     url[UrlParser.PASSWORD] = self._get_resource_attribute(saved_config.saved_artifact.ftp_resource, 'Password')
+        #
+        # if hasattr(params, 'custom_params'):
+        #     if hasattr(params.custom_params, 'path'):
+        #         restore_params['path'] = params.custom_params.path
+        #     if hasattr(params.custom_params, 'configuration_type'):
+        #         restore_params['configuration_type'] = params.custom_params.configuration_type
+        #     if hasattr(params.custom_params, 'restore_method'):
+        #         restore_params['restore_method'] = params.custom_params.restore_method
+        #     if hasattr(params.custom_params, 'vrf_management_name'):
+        #         restore_params['vrf_management_name'] = params.custom_params.vrf_management_name
+        # else:
+        #     restore_params['restore_method'] = 'override'
+        #     restore_params['configuration_type'] = 'running'
+        #     if UrlParser.FILENAME in url and url[UrlParser.FILENAME] and 'startup' in url[UrlParser.FILENAME]:
+        #         restore_params['configuration_type'] = 'startup'
+        #     restore_params['vrf_management_name'] = self._get_resource_attribute(self.resource_name,
+        #                                                                          'VRF Manaagement Name') or ''
+        # try:
+        #     restore_params['path'] = UrlParser.build_url(**url)
+        # except Exception as e:
+        #     self.logger.error('Failed to build url: {}'.format(e))
+        #     if 'path' not in restore_params and not restore_params['path']:
+        #         raise Exception('ConfigurationOperations', 'custom_params is None or empty')
+        #
+        # self.restore(**restore_params)
+        pass
 
     def _validate_artifact_info(self, saved_config):
         """Validate action from the request json, according to APPLY_CONNECTIVITY_CHANGES_ACTION_REQUIRED_ATTRIBUTE_LIST
